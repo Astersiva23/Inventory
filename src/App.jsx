3b2234de8b42092bd4c2968d715764a30a9297ca
Siva M <119498301+Astersiva23@users.noreply.github.com>
2023-12-27 22:38:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ChickenShop from './Components/Inventory'
+
+function App() {
+  
+
+  return (
+    <div>
+      <ChickenShop></ChickenShop>
+    </div>
+  )
+}
+
+export default App
